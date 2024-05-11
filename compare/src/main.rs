3efcb94af8d4a2ada51f1ca86bc06a24ac88a372
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-05-11 13:03:00 +0500

    modified:   compare/.gitignore modified:   compare/src/main.rs modified:   compare/target/.rustc_info.json modified:   compare/target/debug/compare.exe modified:   compare/target/debug/compare.pdb modified:   compare/target/debug/deps/compare.exe modified:   compare/target/debug/deps/compare.pdb modified:   simple_game/target/.rustc_info.json

--- compare/src/main.rs
+++ compare/src/main.rs
@@ -20,7 +20,8 @@ fn main() {
     }
         //taking randm num
     let _magicnum = rand::thread_rng().gen_range(1..=25);
-    println!("Please enter your number");
+    loop {
+        println!("Please enter your number");
     let _usernum = get_inp_int();
     println!("This is your guessed number : \n {_usernum} \n");
     println!("This is the random number : \n {_magicnum}\n");
@@ -28,7 +29,12 @@ fn main() {
   match _magicnum.cmp(&_usernum){
     Ordering::Less => println!("this is the low"),
     Ordering::Equal => println!("this is the Equal"),
-    Ordering::Greater => println!("this is the Greater")
+    Ordering::Greater => {println!("this is the Greater")
+    ;println!("You won");
+    break;}
+        
+    }
+    
   }
 
 
